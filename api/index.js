6e7c5307e552f883ccc21520ebb36a1d8328894c
JUkhan <jasim.uddin.khan@gmail.com>
2020-03-20 21:25:11 +0600

    event and referral map icon

--- api/index.js
+++ api/index.js
@@ -23,13 +23,18 @@ app.get("/getInvitationInfo/:token", (req, res) => {
     data.title1 = "John is inviting you to join kickit";
     data.title2 = "Checkout John's upcoming plans on the kickit app!";
     data.location = { lat: 23.8103, lng: 90.4125 };
-    data.image = "https://via.placeholder.com/50/24f355";
+    data.image = "/images/test.png";
     data.category = "referral";
   } else if (req.params.token.includes("event")) {
     data.title1 = 'You\'ve been invited to "Pregame"';
-    data.address = "address";
-    data.date = "date and time";
-    data.images = ["img1", "img2"];
+    data.address = "1103 Biscayne Blvd, Miami, FL 33132";
+    data.eventClass = "pregame";
+    data.date = new Date().toUTCString();
+    data.images = [
+      "https://via.placeholder.com/50/24f355",
+      "https://via.placeholder.com/50/24f355",
+      "https://via.placeholder.com/50/24f355"
+    ];
     data.title2 = "checkout when a,b,c are ariving to the event";
     data.category = "event";
   }
--- funnel-jquery-plugin/funnel.js
+++ funnel-jquery-plugin/funnel.js
@@ -3,17 +3,61 @@
     var myLatLng = data.location;
 
     var map = new google.maps.Map(document.getElementById("map-container"), {
-      zoom: 7,
+      zoom: 12,
       center: myLatLng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
-    new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: data.image
+    var svg = `<svg width="91" height="91" viewBox="0 0 91 91" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g filter="url(#filter0_d)">
+    <path d="M24 42.5C24 30.6259 33.6259 21 45.5 21C57.3741 21 67 30.6259 67 42.5C67 54.3741 57.3741 64 45.5 64H29.8636C26.6252 64 24 61.3748 24 58.1364V42.5Z" fill="white"/>
+    </g>
+    <circle cx="45.5" cy="42.5" r="18.5857" fill="url(#pattern0)" stroke="white" stroke-width="4"/>
+    <defs>
+    <filter id="filter0_d" x="0" y="0" width="91" height="91" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+    <feOffset dy="3"/>
+    <feGaussianBlur stdDeviation="12"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+    </filter>
+    <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+    <use xlink:href="#image0" transform="translate(0 -0.247664) scale(0.00934579)"/>
+    </pattern>
+    <image id="image0" width="107" height="160" xlink:href="{{imageData}}"/>
+    </defs>
+    </svg>
+    `;
+    toDataURL(data.image).then(res => {
+      var icon = {
+        // url: "/images/friendPin.svg"
+        url:
+          "data:image/svg+xml;charset=UTF-8," +
+          encodeURIComponent(svg.replace("{{imageData}}", res))
+        //anchor: new google.maps.Point(25, 50),
+        //scaledSize: new google.maps.Size(50, 50)
+      };
+      new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon
+        //icon: data.image
+      });
     });
   }
+  const toDataURL = url =>
+    fetch(url)
+      .then(response => response.blob())
+      .then(
+        blob =>
+          new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+          })
+      );
   function injectSDK(mapApiKey) {
     return new Promise(resolve => {
       window["mapInit"] = () => {
@@ -101,9 +145,17 @@
   }
   function renderEvent(el, data) {
     setButtonText(el, "RSVP AND DOWNLOAD");
-    var str = `<div>
-    <p class="text-center font-weight-bold mt-2">${data.title1}</p>
-    <p>Other info goes here</p>
+    var str = `<div class="pt-2 container">
+    <div class="event">
+    <div class="text-right"><i class="${
+      data.eventClass
+    }"/></div><span class="font-weight-bold mt-2">${data.title1}</span>
+    <i/><div><i class="address"/> <span>${data.address}<span></div>
+    <i/><div class="mt-1"><i class="calender"/> <span>${data.date}</span></div>
+    <i/><div>Going: ${data.images
+      .map(i => `<img src="${i}" class="image p-1"/>`)
+      .join("")}</div>
+    </div>
     <p class="text-center">${data.title2}</p>
   </div>`;
     el.prepend($(str));
